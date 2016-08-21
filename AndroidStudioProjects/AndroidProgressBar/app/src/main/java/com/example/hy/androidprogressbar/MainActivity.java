e3360dec3d8e22b86cc447260314f786728b1999
whfwudi <83340224@qq.com>
2016-08-21 22:14:49 +0800

    no message

--- AndroidStudioProjects/AndroidProgressBar/app/src/main/java/com/example/hy/androidprogressbar/MainActivity.java
+++ AndroidStudioProjects/AndroidProgressBar/app/src/main/java/com/example/hy/androidprogressbar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hy.androidprogressbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,14 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar1);
+        final SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar1);
         final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar1);
+        final RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar1);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                float rate = ratingBar.getRating();
+
+                progressBar.setProgress((int) rate*20);
+
+            }
+        });
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressBar.setProgress(seekBar.getProgress());
-                
             }
 
             @Override
